04a904891282e951bb9aa7a078866b6ddfb8663a
felipeffranco <felipeffranco@outlook.com>
2024-01-07 23:16:45 -0300

    throw exceptions in C#

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -14,28 +14,32 @@
 
 
 //PROGRAMA EXCECOES
-try
-{
-    string[] linhas = File.ReadAllLines("Arquivos/arquivo.txt");
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivo.txt");
 
-    foreach(string linha in linhas) 
-    {
-        Console.WriteLine(linha);
-    }
-} 
-catch(FileNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
-}
-catch(DirectoryNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
-}
-catch(Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
-}
-finally
-{
-    Console.WriteLine($"Chegou aqui");
-}
+//     foreach(string linha in linhas) 
+//     {
+//         Console.WriteLine(linha);
+//     }
+// } 
+// catch(FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// catch(DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine($"Chegou aqui");
+// }
+
+
+//THROW
+new ExemploExcecao().Metodo1();
