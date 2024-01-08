58aa711c3bacfda6f7fc8e55600b08108505aca3
felipeffranco <felipeffranco@outlook.com>
2024-01-07 23:25:14 -0300

    queue and stack to represent fila (FIFO) and pilha (LIFO)

--- ExemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
+++ ExemploFundamentos/obj/Debug/net8.0/exemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb9a8afaa7a4ee37f6ff98918ba56ddb7aaa9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a904891282e951bb9aa7a078866b6ddfb8663a")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
